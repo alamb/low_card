b817fee7e8e384821a9a82b003fc1903f428ca42
Andrew Lamb <andrew@nerdnetworks.org>
2022-06-24 11:06:02 -0400

    more

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,74 @@
 
 use std::sync::atomic::{AtomicI64, Ordering};
 
+use futures::{stream::FuturesUnordered, StreamExt};
+use reqwest::Response;
+
 
 
 #[tokio::main]
 async fn main() {
     println!("Hello, starting....");
 
-    let client = reqwest::Client::new();
-
-    let org = "26f7e5a4b7be365b";
-    let bucket = "917b97a92e883afc";
-
-    let params = [("org", org),("bucket", bucket)];
 
     let generator = LineProtoGenerator::new();
+    let client = WriteClient::new();
 
-    let res = client.post("http://localhost:8080/api/v2/write")
-        .query(&params)
-        .body(generator.make_lines(1))
-        .send()
-        .await
-        .expect("request");
+    let futures = (0..10).map(|_| {
+        client.post(generator.make_lines(10))
+    })
+        .collect::<FuturesUnordered<_>>()
+        .collect::<Vec<_>>()
+        .await;
 
-    if res.status().is_success() {
-        println!("Success!");
-    }
-    else {
-        println!("Failure");
-        println!("{:#?}", res);
+
+    for res in futures {
+        if res.status().is_success() {
+            println!("Success!");
+        }
+        else {
+            println!("Failure");
+            println!("{:#?}", res);
+        }
     }
 
 
     println!("done");
 }
+
+/// wrapper that send data to IOx
+#[derive(Debug)]
+struct WriteClient {
+    client: reqwest::Client,
+}
+
+impl WriteClient {
+    fn new() -> Self {
+        let client = reqwest::Client::new();
+
+
+        Self {
+            client,
+        }
+    }
+
+    async fn post(&self, data: Vec<u8>) -> Response {
+        let org = "26f7e5a4b7be365b";
+        let bucket = "917b97a92e883afc";
+        let params = vec![("org", org),("bucket", bucket)];
+
+        println!("Sending {} bytes of data...", data.len());
+        self.client.post("http://localhost:8080/api/v2/write")
+            .query(&params)
+            .body(data)
+            .send()
+            .await
+        .expect("request failed")
+    }
+
+}
+
+
 
 
 /// Makes pathalogical line protocol
